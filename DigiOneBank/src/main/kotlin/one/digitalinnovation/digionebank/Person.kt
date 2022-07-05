e4f3585865cb518b2a03c9545c11712626226f2b
Geisson de Oliveira <geissonlucaso@gmail.com>
2022-07-05 13:21:16 -0300

    Updated to apply the private modifier to set function of the cfp property.

--- DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
+++ DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
@@ -2,10 +2,13 @@ package one.digitalinnovation.digionebank
 
 class Person {
     var name:String = "Geisson"
+
     var cpf:String = "123.456.789-12"
+    private set
+
 }
 
-fun main(){
+fun main() {
     val person = Person()
 
     println(person.name)
