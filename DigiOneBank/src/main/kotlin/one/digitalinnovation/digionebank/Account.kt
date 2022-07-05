a5dba961822188b58463a53ccea7fa5acdaf2c2b
Geisson de Oliveira <geissonlucaso@gmail.com>
2022-07-05 14:48:07 -0300

    Create Account class.

---
+++ DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/Account.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Account(
+     val agency:String,
+     val number:String,
+     val balance:BigDecimal
+) {
+    fun deposit(cash:BigDecimal){
+
+    }
+
+    fun withdraw(cash:BigDecimal){
+
+    }
+
+}
